d3573d9225e5cd750843b1980e6946d0973226e8
unknown <juanipicart@gmail.com>
2019-10-12 20:27:38 -0300

    Comiteando test

--- PDTEaeTechGroup2/src/Sources.java
+++ PDTEaeTechGroup2/src/Sources.java
@@ -4,7 +4,7 @@ public class Sources {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String hi = "Probando una branch";
+		String hi = "Probando una branchhhhh";
 		
 
 	}
