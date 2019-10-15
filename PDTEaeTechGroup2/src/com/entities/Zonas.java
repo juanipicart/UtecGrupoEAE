2bf3400d3912908a826880367343147ac4b07c92
robertbozzo <rbozzo@gmail.com>
2019-10-14 23:15:28 -0300

    Creacion de entidad zonas

---
+++ PDTEaeTechGroup2/src/com/entities/Zonas.java
@@ -0,0 +1,47 @@
+package com.entities;
+
+import java.io.Serializable;
+import java.lang.Number;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Zonas
+ *
+ */
+@Entity
+@Table(name ="ZONAS")
+
+public class Zonas implements Serializable {
+
+	// Se indica la secuencia, valor de crecimiento y un identificador
+	@Id
+	@SequenceGenerator(name = "idzona", sequenceName = "id_zona_seq", allocationSize = 1) 
+	@GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "idzona")
+	@Column(name = "ID_ZONA")
+	private Number id_zona;
+	
+	@Column(name = "DESCRIPCION")
+	private String Descripcion;
+	
+	private static final long serialVersionUID = 1L;
+
+	public Zonas() {
+		super();
+	}   
+	public Number getId_zona() {
+		return this.id_zona;
+	}
+
+	public void setId_zona(Number id_zona) {
+		this.id_zona = id_zona;
+	}   
+	public String getDescripcion() {
+		return this.Descripcion;
+	}
+
+	public void setDescripcion(String Descripcion) {
+		this.Descripcion = Descripcion;
+	}
+   
+}
