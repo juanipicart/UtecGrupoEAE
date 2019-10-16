4db3f499834f49d75f3177c8bb3092ef271aceac
Robert Bozzo <50419515+RobertBozzo@users.noreply.github.com>
2019-10-16 00:10:50 -0300

    subo version de ubicacion

--- PDTEaeTechGroup2/src/com/entities/Ubicacion.java
+++ PDTEaeTechGroup2/src/com/entities/Ubicacion.java
@@ -3,25 +3,69 @@ package com.entities;
 import java.io.Serializable;
 import javax.persistence.*;
 
+
 /**
- * Entity implementation class for Entity: Ubicación
- *
+ * The persistent class for the UBICACIONES database table.
+ * 
  */
 @Entity
-@Table(name="Ubicaciones")
-
+@Table(name = "UBICACIONES")
+@NamedQuery(name="Ubicaciones.findAll", query="SELECT u FROM Ubicacion u")
 public class Ubicacion implements Serializable {
-
-	
 	private static final long serialVersionUID = 1L;
 
-	public Ubicacion() {
-		super();
-	}
-	
 	@Id
-	@GeneratedValue
-	@Column(name = "ID_UBICACION")
-	private Long id_ubicacion;
-   
+	@SequenceGenerator(name="UBICACIONES_IDUBICACION_GENERATOR", sequenceName="ID_UBICACION_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="UBICACIONES_IDUBICACION_GENERATOR")
+	@Column(name="ID_UBICACION")
+	private long idUbicacion;
+
+	@Column(name="ID_ZONA")
+	private java.math.BigDecimal idZona;
+
+	//bi-directional many-to-one association to Departamentos
+	@ManyToOne
+	@JoinColumn(name="ID_DEPARTAMENTO")
+	private Departamento departamento;
+
+	//bi-directional many-to-one association to Localidades
+	@ManyToOne
+	@JoinColumn(name="ID_LOCALIDAD")
+	private Localidades localidade;
+
+	public Ubicacion() {
+	}
+
+	public long getIdUbicacion() {
+		return this.idUbicacion;
+	}
+
+	public void setIdUbicacion(long idUbicacion) {
+		this.idUbicacion = idUbicacion;
+	}
+
+	public java.math.BigDecimal getIdZona() {
+		return this.idZona;
+	}
+
+	public void setIdZona(java.math.BigDecimal idZona) {
+		this.idZona = idZona;
+	}
+
+	public Departamento getDepartamento() {
+		return this.departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public Localidades getLocalidade() {
+		return this.localidade;
+	}
+
+	public void setLocalidade(Localidades localidade) {
+		this.localidade = localidade;
+	}
+
 }
